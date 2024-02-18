91380ad60010fdd0a8b220a726c00f935a9d1731
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-02-18 23:25:03 +0600

    skill done

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,9 +1,35 @@
 import styles from '../../assets/css/Skills.module.css'
+import { FaPaintBrush } from "react-icons/fa";
+import { MdDeveloperMode } from "react-icons/md";
+import { HiDeviceTablet } from "react-icons/hi";
 const Skills = () => {
     return (
         <div className={styles.skillContent}>
        <h1 className={styles.heading}>My <span>Skills</span></h1>
         <div className={styles.skillPage}>
+           <div>
+           <div className={styles.skillTextContent}>
+                <a href="#"><FaPaintBrush /></a>
+                <div className={styles.skillText}>
+                    <h3>Development</h3>
+                    <p>clean, modern, optimized for performance, search engines, and converting users to customers</p>
+                </div>
+            </div>
+            <div className={styles.skillTextContent}>
+                <a href="#"><MdDeveloperMode /></a>
+                <div className={styles.skillText}>
+                    <h3>Technology</h3>
+                    <p>combined all the latest technologies to a progressive website</p>
+                </div>
+            </div>
+            <div className={styles.skillTextContent}>
+                <a href="#"><HiDeviceTablet /></a>
+                <div className={styles.skillText}>
+                    <h3>always responsive</h3>
+                    <p>a responsive design makes your website to all user regardless of their divice</p>
+                </div>
+            </div>
+           </div>
             <div className={styles.skillContainer}>
             <div className={styles.skills}>
                 <div className={`${styles.innerSkill} ${styles.innerSkillHTML}`}>
